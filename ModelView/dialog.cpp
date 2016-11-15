fefecf001d8ccefdcb567804e86198190d6d218d
bruno boutin <b.boutin@uva.nl>
2016-11-15 13:25:45 +0100

    Add tata & toto in the list

--- ModelView/dialog.cpp
+++ ModelView/dialog.cpp
@@ -7,7 +7,7 @@ Dialog::Dialog(QWidget *parent) :
 {
 	ui->setupUi(this);
 	QStringList list;
-    list << "aap" << "noten" << "mies!";
+    list << "aap" << "noten" << "mies!" << "toto" << "titi";
 	_model = new QStringListModel(this);
 	_model->setStringList(list);
 	ui->comboBox->setModel(_model);
