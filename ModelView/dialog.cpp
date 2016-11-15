a41a03cd4379e11f0d96ba45f666d1ea8da4bebe
FransGmail <Frans.Meerhoff@gmail.com>
2016-11-15 14:38:42 +0100

    Development (#10)
    * Remove exclamation
    
    * Remove exclamation

--- ModelView/dialog.cpp
+++ ModelView/dialog.cpp
@@ -7,7 +7,7 @@ Dialog::Dialog(QWidget *parent) :
 {
 	ui->setupUi(this);
 	QStringList list;
-    list << "aap" << "noten" << "mies!" << "toto" << "tata";
+	list << "aap" << "noten" << "mies" << "toto" << "tata";
 	_model = new QStringListModel(this);
 	_model->setStringList(list);
 	ui->comboBox->setModel(_model);
